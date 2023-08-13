4b30716fc0c5a6279cc58846d188618b8c9692ca
Nayer Kotry <nayer.kotry@gmail.com>
2023-08-13 19:09:01 +0300

    Initial Commit

---
+++ MovieAPI/ViewModel/GenreViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GenreViewModel.swift
+//  MovieAPI
+//
+//  Created by Nayer Kotry on 28/08/2023.
+//
+
+import Foundation
---
+++ MovieAPI/ViewModel/MovieViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieViewModel.swift
+//  MovieAPI
+//
+//  Created by Nayer Kotry on 28/08/2023.
+//
+
+import Foundation
